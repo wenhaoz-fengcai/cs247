3f9efac0f3e711aa78f1408ac04420b82498556d
Noor Nakhaei <50219342+noornk@users.noreply.github.com>
2019-05-17 16:33:19 -0700

    Update search.py

--- src/search.py
+++ src/search.py
@@ -1,33 +1,41 @@
 from SPARQLWrapper import SPARQLWrapper, CSV
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.addDefaultGraph("http://dbpedia.org")
-# this list is for test, Should be replaced with the list of the words we found in the previous step!
-mylist = [("London", "Location"), ("Obama", "Person"), ("UNICEF", "Organization"), ("jhfvjhegrfvh", "Location"), ("Noor_Nakhaei", "Person"), ("Center_For_Smart_Health", "Organization")]
-new = []
-existing = []
-# we search in the dbpedia to see if the words exist
-for i in mylist:
-    sparql.setQuery("""
-    SELECT DISTINCT ?item ?label WHERE{
-                ?item rdfs:label ?label .
-                FILTER (lang(?label) = 'en').
-                ?label bif:contains '%s' .
-                ?item dct:subject ?sub
-        }
-    """ % i[0])
-    try:
-        sparql.setReturnFormat(CSV)
-        results = sparql.query()
-        triples = results.convert()
-    except:
-        print("query failed")
-    # if the word exists, we add it to the to existing list
-    if len(triples) > 15:
-        print(triples)
-        existing.append(i)
-    # if the word doesn't exists, we add it to the to new list
-    else:
-        new.append(i)
 
-print(new)
-print(existing)
+
+class Search:
+
+    def __init__(self):
+        self.sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        self.sparql.addDefaultGraph("http://dbpedia.org")
+        self.new = []
+        self.existing = []
+        # this list is for test, Should be replaced with the list of the words we found in the previous step!
+        # mylist = [("London", "Location"), ("Obama", "Person"), ("UNICEF", "Organization"), ("jhfvjhegrfvh", "Location"
+        # ), ("Noor_Nakhaei", "Person"), ("Center_For_Smart_Health", "Organization")]
+
+    def search(self, mylist):
+        # new = []
+        # existing = []
+        # we search in the dbpedia to see if the words exist
+        for i in mylist:
+            self.sparql.setQuery("""
+            SELECT DISTINCT ?item ?label WHERE{
+                        ?item rdfs:label ?label .
+                        FILTER (lang(?label) = 'en').
+                        ?label bif:contains '%s' .
+                        ?item dct:subject ?sub
+                }
+            """ % i[0])
+            try:
+                self.sparql.setReturnFormat(CSV)
+                results = self.sparql.query()
+                triples = results.convert()
+            except:
+                print("query failed")
+            # if the word exists, we add it to the to existing list
+            if len(triples) > 15:
+                print(triples)
+                self.existing.append(i)
+            # if the word doesn't exists, we add it to the to new list
+            else:
+                self.new.append(i)
+        return self.new, self.existing
