dcaab98f8f6e158bddb369ffac689b9832e512bb
jiaowoshabi <wenhaoz@g.ucla.edu>
2019-05-21 16:13:17 -0700

    refactored search.py and add test case for search.py

--- src/search.py
+++ src/search.py
@@ -1,28 +1,32 @@
 from SPARQLWrapper import SPARQLWrapper, CSV
 
-
-class Search:
+class Search(object):
     
     """
     Python class which searches for the entities found in previous step, in the dbpedia database.
         If the entities were not found, they're declared in a list as new entities.
     Attributes:
-        entities: a list of tuples: words, and they're type.
         new: a list of new entities.
         existing: a list of existing entities.
-        sparql: This is a wrapper around a SPARQL service. It helps in creating the query URI and, possibly,
-            convert the result into a more manageable format.
+        sparql: This is a wrapper around a SPARQL service. It helps in creating the query URI and, possibly, convert the result into a more manageable format.
     """
     def __init__(self):
         self.sparql = SPARQLWrapper("http://dbpedia.org/sparql")
         self.sparql.addDefaultGraph("http://dbpedia.org")
         self.new = []
         self.existing = []
-        # this list is for test, Should be replaced with the list of the words we found in the previous step!
-        # mylist = [("London", "Location"), ("Obama", "Person"), ("UNICEF", "Organization"), ("jhfvjhegrfvh", "Location"
-        # ), ("Noor_Nakhaei", "Person"), ("Center_For_Smart_Health", "Organization")]
 
-    def search(self, entities):
+
+    def query(self, entities):
+        """
+        This function search in the DBpedia and checks if entities exist in knowledge base.
+
+        Args:
+            entities: a list of tuples; words, and their labels.
+
+        Returns:
+            Returns a list of new enetities which are words not in the DBpedia, and a list of contextual words (uni-gram words) exists in DBpedia. Entities in both lists are considered as nodes in Heterogeneous Textual Graph.
+        """
         # we search in the dbpedia to see if the words exist
         for i in entities:
             self.sparql.setQuery("""
@@ -41,7 +45,7 @@ class Search:
                 print("query failed")
             # if the word exists, we add it to the to existing list
             if len(triples) > 15:
-                print(triples)
+                # print(triples)
                 self.existing.append(i)
             # if the word doesn't exists, we add it to the to new list
             else:
