3a28a87b4fbe13c99a904a6ca3e9055a0435ee05
Eric Dang <edang830@gmail.com>
2019-05-31 16:03:19 -0700

    the secondary classifier

---
+++ src/f_classifier.py
@@ -0,0 +1,12 @@
+from g_classifier import G_Classifier
+
+class F_Classifier():
+    """
+    A class for the secondary classifier that will derive the final predictions (i.e. reveal emerging relations) from
+    results of the G_Classifier.
+    """
+
+
+    def __init__(self):
+        pass
+
