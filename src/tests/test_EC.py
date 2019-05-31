f20c95508737651e6331867c5e3e11711c8c95a0
jiaowoshabi <wenhaoz@g.ucla.edu>
2019-05-31 16:38:29 -0700

    fix issue in EC graph, where all links between entity-entity and cword-entity removed

--- src/tests/test_EC.py
+++ src/tests/test_EC.py
@@ -1,22 +1,25 @@
 import unittest
 from src.ec_graph import EC
 
-NEWS = ["Trump demands Hazzaaaa"]
+NEWS = ["Trump demands hazzaaaa"]
 
 class TestEC(unittest.TestCase):
 
     def test_init(self):
-        self.ec = EC(NEWS, window_size=2)
+        self.ec = EC(NEWS, window_size=3)
         nodes = self.ec.get_nodes()
-        assert set(nodes) == {"Trump", "demands", "Hazzaaaa"}
+        assert set(nodes) == {"Trump", "demands", "hazzaaaa"}
         edges = self.ec.get_edges()
         assert set(edges) == {("Trump", "demands"),
-                              ("demands", "Hazzaaaa")}
+                              ("Trump", "hazzaaaa")}
         df = self.ec.get_weights()
+        print(df)
         assert df.loc["Trump", "demands"] == 1
         assert df.loc["demands", "Trump"] == 1
-        assert df.loc["Hazzaaaa", "demands"] == 1
-        assert df.loc["demands", "Hazzaaaa"] == 1
+        assert df.loc["Trump", "hazzaaaa"] == 1
+        assert df.loc["hazzaaaa", "Trump"] == 1
+        assert df.loc["demands", "hazzaaaa"] == 0
+        assert df.loc["hazzaaaa", "demands"] == 0
 
 if __name__ == "__main__":
     unittest.main()
