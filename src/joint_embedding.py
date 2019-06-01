d36ff1007b722b7764c3c21b2866fe93411558da
Noor Nakhaei <50219342+noornk@users.noreply.github.com>
2019-06-01 12:51:53 -0700

    Add files via upload

---
+++ src/joint_embedding.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import random
+from random import randint
+from src.reader import Reader
+from src.search import Search
+from src.graph import Graph
+from src.ee_graph import EE
+from src.ec_graph import EC
+from src.cc_graph import CC
+from src.kg_graph import KG
+from numpy.linalg import inv
+
+
+class Embedding(object):
+    """
+
+    """
+    def __init__(self, heer, kg):
+
+        self.S = pd.DataFrame()
+        self.T = pd.DataFrame()
+        self.ee_graph = 1
+        self.ec_graph = 1
+        self.cc_graph = 1
+        self.kg_graph = 1
+
+    def weighted_sample(self, items, n):
+        total = float(sum(w for w, v in items))
+        i = 0
+        w, v = items[0]
+        while n:
+            x = total * (1 - random.random() ** (1.0 / n))
+            total -= x
+            while x > w:
+                x -= w
+                i += 1
+                w, v = items[i]
+            w -= x
+            yield v
+            n -= 1
+
+    def embedding_update(self, s, t, g=Graph, k=10, n=0):
+        eta = 0.2
+        edges = g.get_edges()
+        # Sample an edge from G and draw k negative edges
+        # and I guess, when we sample an edge, we also update that node's weight in the embedding!
+        # So for sampling I should have all the weights,
+
+        # items = [(10, "low"),
+        #          (100, "mid"),
+        #          (890, "large")]
+        items = [(10, ('a', 'b')),
+                 (100, ('c', 'b')),
+                 (890, ('d', 'c'))]
+
+        edges_weights = []
+        edges = []
+        warning = '!'
+        sampled_edge = self.weighted_sample(items, 1)
+        sampled_node_a = sampled_edge[0]
+        sampled_node_b = sampled_edge[1]
+        #swap!
+        if s == 1 and t == 1:
+            if type(sampled_node_a) == tuple:
+                s1 = sampled_node_b
+                sampled_node_b = sampled_node_a
+                sampled_node_a = s1
+        if s == 1 and t == 1:
+            s1 = sampled_edge[0]
+            s2 = sampled_edge[1]
+        if s == t:
+            nodes = g.get_words()
+        else:
+            nodes = g.get_nodes() # so I think we should have all the nodes! => get_nodes()
+        # draw k negative edges!
+        sampled_edge_nodes = random.sample(nodes, k) # [k]
+
+        #so up until here, we have k negative edges, one positive edge, the graph, and S_t, T_t
+        if s == 1 and t == 1:  # S, T, G_ec
+            sum = 0
+            for i in k+1:
+                a = np.dot(self.S[sampled_edge[i]], self.T[sampled_node_a])
+                b = np.exp(a)
+                sum = sum + b
+            c = np.log(sum)
+            d = inv(self.S[sampled_node_b])
+            e = - eta * d * c
+            self.T[sampled_node_a] = self.T[sampled_node_a] - e
+            sum = 0
+            for i in k+1:
+                a = np.dot(self.S[sampled_edge[i]], self.T[sampled_node_a])
+                b = np.exp(a)
+                sum = sum + b
+            c = np.log(sum)
+            d = inv(self.T[sampled_node_a])
+            e = - eta * d * c
+            self.S[sampled_node_b] = self.S[sampled_node_b] - e
+        elif s == 0 and t == 1:  # T, T, G_cc
+            sum = 0
+            for i in k+1:
+                a = np.dot(self.T[sampled_edge[i]], self.T[sampled_node_a])
+                b = np.exp(a)
+                sum = sum + b
+            c = np.log(sum)
+            d = inv(self.T[sampled_node_b])
+            e = - eta * d * c
+            self.T[sampled_node_a] = self.T[sampled_node_a] - e
+        elif s == 1 and t == 0:  # S, S, G_ee
+            sum = 0
+            for i in k+1:
+                a = np.dot(self.S[sampled_edge[i]], self.S[sampled_node_a])
+                b = np.exp(a)
+                sum = sum + b
+            c = np.log(sum)
+            d = inv(self.S[sampled_node_a])
+            e = - eta * d * c
+            self.S[sampled_node_b] = self.S[sampled_node_b] - e
+
+    def joint_embedding(self):
+        # I'll have the graph's, this is just initializing
+        # graph_ee = nx.fast_gnp_random_graph(n=100, p=0.5)
+        # graph_ec = nx.fast_gnp_random_graph(n=100, p=0.5)
+        # graph_cc = nx.fast_gnp_random_graph(n=100, p=0.5)
+        # graph_kg = nx.fast_gnp_random_graph(n=100, p=0.5)
+        # the guiding parameter, which we should have empirically
+        theta = 0.8
+        # number of negative samplings
+        k = 10
+        # number of iterations
+        t = 100
+
+        # initializing the embeddings
+        # Sampling would be like this: (though I'm not sure about the numbers)
+        # S = pd.DataFrame(randint(0, 100), index=(0, 100), columns=(0, 100))
+        # T = pd.DataFrame(randint(0, 100), index=(0, 100), columns=(0, 100))
+        # the loop of the EM algorithm
+        while t > 0:
+            gamma = random.uniform(0, 1)
+            if gamma <= theta:
+                self.embedding_update(self.S, self.S, self.kg_graph, k, 1)
+            else:
+                self.embedding_update(self.S, self.T, self.ec_graph, k, 2)
+                self.embedding_update(self.S, self.S, self.ee_graph, k, 3)
+                self.embedding_update(self.T, self.T, self.cc_graph, k, 4)
+            t = t - 1
