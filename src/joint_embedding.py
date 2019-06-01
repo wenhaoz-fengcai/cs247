ba3ba86b2d6b2d563d8fcb4480e6f3cc18529aad
Noor Nakhaei <50219342+noornk@users.noreply.github.com>
2019-06-01 13:53:24 -0700

    Update joint_embedding.py

--- src/joint_embedding.py
+++ src/joint_embedding.py
@@ -21,7 +21,7 @@ class Embedding(object):
 
         self.read = Reader()
         self.search = Search()
-        self.news_list = self.read.read_csv_file("../data/mixed-news/articles-title_only.csv")
+        self.news_list = self.read.read_csv_file("./data/mixed-news/articles-title_only.csv")
         self.entity = self.read.parse_news(self.news_list[0:1])
         self.new, self.exist = self.search.query(self.entity)
         self.graph = Graph(self.news_list)
@@ -54,10 +54,6 @@ class Embedding(object):
         # Sample an edge from G and draw k negative edges
         # and I guess, when we sample an edge, we also update that node's weight in the embedding!
         # So for sampling I should have all the weights,
-
-        edges_weights = []
-        edges = []
-        warning = '!'
         sampled_edge = self.weighted_sample(g.get_edges(), 1)
         sampled_node_a = sampled_edge[0]
         sampled_node_b = sampled_edge[1]
@@ -67,13 +63,14 @@ class Embedding(object):
                 s1 = sampled_node_b
                 sampled_node_b = sampled_node_a
                 sampled_node_a = s1
-        samplede_neg_nodes = []
-        for i in k:
-            if s == 1 and t == 1:
-                samplede_neg_nodes[i] = g.get_entities()
-            else:
-                samplede_neg_nodes[i] = g.get_nodes()
-        samplede_neg_nodes[k+1] = sampled_node_b
+        # sampled_neg_nodes = []
+        if s == 1 and t == 1:
+            nodes = g.get_entities()
+        else:
+            nodes = g.get_nodes()
+        # draw k negative edges!
+        sampled_neg_nodes = random.sample(nodes, k) # [k]
+        sampled_neg_nodes[k+1] = sampled_node_b
         # if s == 1 and t == 1:
         #     s1 = sampled_edge[0]
         #     s2 = sampled_edge[1]
@@ -88,7 +85,7 @@ class Embedding(object):
         if s == 1 and t == 1:  # S, T, G_ec
             sum = 0
             for i in k+1:
-                a = np.dot(self.S[sampled_edge[i]], self.T[sampled_node_a])
+                a = np.dot(self.S[sampled_neg_nodes[i]], self.T[sampled_node_a])
                 b = np.exp(a)
                 sum = sum + b
             c = np.log(sum)
@@ -97,7 +94,7 @@ class Embedding(object):
             self.T[sampled_node_a] = self.T[sampled_node_a] - e
             sum = 0
             for i in k+1:
-                a = np.dot(self.S[sampled_edge[i]], self.T[sampled_node_a])
+                a = np.dot(self.S[sampled_neg_nodes[i]], self.T[sampled_node_a])
                 b = np.exp(a)
                 sum = sum + b
             c = np.log(sum)
@@ -107,7 +104,7 @@ class Embedding(object):
         elif s == 0 and t == 1:  # T, T, G_cc
             sum = 0
             for i in k+1:
-                a = np.dot(self.T[sampled_edge[i]], self.T[sampled_node_a])
+                a = np.dot(self.T[sampled_neg_nodes[i]], self.T[sampled_node_a])
                 b = np.exp(a)
                 sum = sum + b
             c = np.log(sum)
@@ -117,7 +114,7 @@ class Embedding(object):
         elif s == 1 and t == 0:  # S, S, G_ee
             sum = 0
             for i in k+1:
-                a = np.dot(self.S[sampled_edge[i]], self.S[sampled_node_a])
+                a = np.dot(self.S[sampled_neg_nodes[i]], self.S[sampled_node_a])
                 b = np.exp(a)
                 sum = sum + b
             c = np.log(sum)
@@ -126,22 +123,13 @@ class Embedding(object):
             self.S[sampled_node_b] = self.S[sampled_node_b] - e
 
     def joint_embedding(self):
-        # I'll have the graph's, this is just initializing
-        # graph_ee = nx.fast_gnp_random_graph(n=100, p=0.5)
-        # graph_ec = nx.fast_gnp_random_graph(n=100, p=0.5)
-        # graph_cc = nx.fast_gnp_random_graph(n=100, p=0.5)
-        # graph_kg = nx.fast_gnp_random_graph(n=100, p=0.5)
         # the guiding parameter, which we should have empirically
         theta = 0.8
         # number of negative samplings
         k = 10
         # number of iterations
         t = 100
-        # initializing the embeddings
-        # Sampling would be like this: (though I'm not sure about the numbers)
-        # S = pd.DataFrame(randint(0, 100), index=(0, 100), columns=(0, 100))
-        # T = pd.DataFrame(randint(0, 100), index=(0, 100), columns=(0, 100))
-        # the loop of the EM algorithm
+        # the loop of the algorithm
         while t > 0:
             gamma = random.uniform(0, 1)
             if gamma <= theta:
