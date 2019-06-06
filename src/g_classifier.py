3dcfdb15b89df0a75af16678bd7dcd7b717dd401
Eric Dang <edang830@gmail.com>
2019-06-05 17:33:52 -0700

    fix bugs

--- src/g_classifier.py
+++ src/g_classifier.py
@@ -4,6 +4,7 @@ from itertools import combinations
 import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
 
 from src.ee_graph import EE
 from src.kg_graph import KG
@@ -29,7 +30,7 @@ class G_Classifier():
         emerging_relations: The predict emerging relations after filtering results_df
     """
 
-    def __init__(self, architecture=(10,10,10), sample_size=0.25, threshold=0.7):
+    def __init__(self, architecture=(10,10,10), sample_size=0.25, threshold=0.91):
         """
         Constructor
 
@@ -39,18 +40,41 @@ class G_Classifier():
             sample_size (float, optional): The fraction of total samples to randomly sample in order to estimate epsilon. Defaults to 0.25.
             threshold (float, optional): The probability threshold to be considered an emerging relation. Defaults to 0.7.
         """
-        self.embedder = Embedding()
-        self.ee_graph = self.embedder.ee_graph
-        self.kg_graph = self.embedder.kg_graph
+        # self.embedder = Embedding()
+        # self.ee_graph = self.embedder.ee_graph
+        # self.kg_graph = self.embedder.kg_graph
         self.classifier = MLPClassifier(hidden_layer_sizes=architecture)
+        self.classifier = LogisticRegression()
 
-        self.embeddings = self.embedder.joint_embedding()[0]
-        self.all_df, self.kg_df, self.new_comb = self.create_combinations(self.ee_graph.get_nodes, self.kg_graph.get_nodes, self.ee_graph.nodes, self.embeddings)
-        self.S_known = self.random_sample(self.all_df, sample_size, minimum_sample)
+        # self.embeddings = self.embedder.joint_embedding()[0]
+        self.embeddings, self.entities, self.kg, self.new = self.parse_csv()
+        # self.all_df, self.kg_df, self.new_comb = self.create_combinations(self.ee_graph.get_nodes, self.kg_graph.get_nodes, self.ee_graph.nodes, self.embeddings)
+        self.all_df, self.kg_df, self.new_comb = self.create_combinations(self.entities, self.kg, self.new, self.embeddings)
+        self.S_known = self.random_sample(self.all_df, sample_size)
         self.classifier = self.train_classifier(self.classifier, self.all_df)
-        self.eps = self.generate_eps(self.S_known)
-        self.results_df = self.predict_emerging_probs(self.classifier, self.eps)
+        self.eps = self.generate_eps(self.classifier, self.S_known)
+        self.results_df = self.predict_emerging_probs(self.classifier, self.all_df, self.eps)
         self.emerging_relations = self.filter_results(self.results_df, self.kg_df, self.new_comb, threshold)
+
+    def parse_csv(csv):
+        with open('entities.csv', 'r') as f:
+            embeddings = {}
+            entities = []
+            f.readline()
+            for line in f.readlines():
+                line = line.split(',')
+                embeddings[line[0]] = [line[1:]]
+                entities.append(line[0])
+        kg = ['Britain', 'Clarke', 'Kingdom', 'Michael', 'Rome', 'Straw', 'Telegraph', 'YouGov'] # np.unique(np.random.choice(entities, size=int(len(entities) * 0.5)))
+        embeddings = pd.DataFrame.from_dict(embeddings, orient='index')
+        new = np.setdiff1d(np.array(entities), kg)
+
+        entities = [(x, []) for x in entities]
+        kg = [(x, []) for x in kg]
+        new = [(x, []) for x in new]
+
+        return embeddings, entities, kg, new
+
 
     def embed_pairs(self, pairs):
         """
@@ -64,14 +88,11 @@ class G_Classifier():
             Dataframe: A df containing the embeddings of the entity-entity pairs as values and indexed by e-e pair
         """
 
-        # Alittle confuse here. So, here the pairs variable with structure [[pair_string,[emb1,emb2]],...,] right?
 
-        # what is means of pair[0] and pair[i][j]? ----Hui
-        #embeddings = {str(pair[0]), self.__h(pair[1][0], pair[1][0]) for pair in pairs}
-        embeddings = {(str(pair[0]), self.h(pair[1][0], pair[1][1])) for pair in pairs}
-        embeddings = pd.from_dict(embeddings, orient='index')
-        embeddings.rename(index=str, columns={'0': 'embedding'})
-        # The return embedding is a dataframe with column are the pairs name and elements are x from the paper?
+        embeddings = {str(pair[0]): [self.h(pair[1][0], pair[1][1])] for pair in pairs}
+        embeddings = pd.DataFrame.from_dict(embeddings, orient='index')
+        embeddings = embeddings.rename(columns={0: 'embedding'})
+
         return embeddings
 
     def create_combinations(self, ee_nodes, kg_nodes, nodes, embeddings):
@@ -82,7 +103,7 @@ class G_Classifier():
         Args:
             ee_nodes (list): A list containing all entities as (word, label) pairs
             kg_nodes (list): A list containing known entites as (word, label) pairs
-            nodes (dict): A dict of all all nodes in ee graph
+            nodes (dict): A dict of all new nodes in ee graph
             embeddings (dataframe): The embeddings of each word indexed by the enitity
 
         Returns:
@@ -94,7 +115,8 @@ class G_Classifier():
         # parse entities from list
         all_entities = list(zip(*ee_nodes))[0]
         kg_entities = list(zip(*kg_nodes))[0]
-        new_entities = list(zip(*nodes['N']))[0]
+        # new_entities = list(zip(*nodes['N']))[0]
+        new_entities = list(zip(*nodes))[0]
 
         # create all combinations of entity-entity pairs
         all_comb = combinations(all_entities, 2)
@@ -102,8 +124,8 @@ class G_Classifier():
         new_comb = combinations(new_entities, 2)
 
         # pair the entity-entity pairs with their embeddings
-        all_comb = [(pair, (embeddings.loc[pair[0], "0"], embeddings.loc[pair[1], "0"])) for pair in all_comb]    # dont know column name in dataframe corresponding to embedding yet
-        kg_comb = [(pair, (embeddings.loc[pair[0], "0"], embeddings.loc[pair[1], "0"])) for pair in kg_comb]    # dont know column name in dataframe corresponding to embedding yet
+        all_comb = [(pair, (embeddings.loc[pair[0], 0], embeddings.loc[pair[1], 0])) for pair in all_comb]    # dont know column name in dataframe corresponding to embedding yet
+        kg_comb = [(pair, (embeddings.loc[pair[0], 0], embeddings.loc[pair[1], 0])) for pair in kg_comb]    # dont know column name in dataframe corresponding to embedding yet
 
         # combine entity-entity pair embedings together
         all_df = self.embed_pairs(all_comb)
@@ -111,7 +133,7 @@ class G_Classifier():
 
         # give proper "z" labels to entity-entity pairs that exist in KG
         all_df.insert(1, 'z', 0)
-        intersection = all_df.Index.intersection(kg_df.Index)
+        intersection = all_df.index.intersection(kg_df.index)
         all_df.loc[intersection, 'z'] = 1
 
         return  all_df, kg_df, new_comb
@@ -128,6 +150,8 @@ class G_Classifier():
             np array: The embedding of the entity-entity pair
         """
 
+        x = [float(n) for n in x]
+        y = [float(n) for n in y]
         return 0.5 * np.add(x, y) # forumula from paper
 
     def random_sample(self, all_df, sample_size):
@@ -142,9 +166,9 @@ class G_Classifier():
             Dataframe: The subset of entity-entity pairs from the random sample that are in the KG
         """
 
-        sample_size = all_df.shape[0] * sample_size
+        sample_size = int(all_df.shape[0] * sample_size)
         S = all_df.sample(n=sample_size)
-        S_known = S.where(S['z'] == 1)
+        S_known = S.where(S['z'] == 1).dropna()
 
         return S_known
 
@@ -160,8 +184,9 @@ class G_Classifier():
             sklearn classifier: The trained classifier
         """
 
-        X = np.array(all_df.loc[:, "embedding"])
-        y = np.array(all_df.loc[:, 'z'])
+        X = all_df.loc[:, "embedding"].as_matrix()
+        X = np.array([x.tolist() for x in X])
+        y = all_df.loc[:, 'z'].as_matrix()
 
         classifier.fit(X, y)
 
@@ -179,7 +204,8 @@ class G_Classifier():
             float: the value of epsilon
         """
 
-        X = np.array(random_sample.loc[:, "embedding"])
+        X = random_sample.loc[:, "embedding"].as_matrix()
+        X = np.array([x.tolist() for x in X])
         size = random_sample.shape[0]
         probs = classifier.predict_proba(X)
 
@@ -194,11 +220,12 @@ class G_Classifier():
             eps (float): The value of epsilon
         """
 
-        X = np.array(all_df.loc[:, 'embedding'])
+        X = all_df.loc[:, 'embedding'].as_matrix()
+        X = np.array([x.tolist() for x in X])
         probs = classifier.predict_proba(X)
         probs = probs / eps
         results_df = all_df.copy()
-        results_df.insert(2, 'probs', probs)
+        results_df.insert(2, 'probs', probs[:, 0])
         return results_df
 
     def filter_results(self, results_df, kg_df, new_comb, threshold):
@@ -215,18 +242,19 @@ class G_Classifier():
         Returns:
             Dataframe: The remaining relations
         """
-
         # get entity-entity pairs that are not in KG
-        intersection = results_df.Index.intersection(kg_df.Index)
+        intersection = results_df.index.difference(kg_df.index)
         results_df = results_df.loc[intersection, :]
 
         # get entity-entity pairs that have at least 1 node in KG
         new_comb = pd.Index(new_comb)
-        intersection = results_df.Index.intersection(new_comb)
+        intersection = results_df.index.difference(new_comb)
+        # print(intersection)
         results_df = results_df.loc[intersection, :]
 
+        # print(results_df)
         # filter results down to entity-entity pairs with probability >= threshold
-        results_df = results_df.where(results_df['probs'] >= threshold)
+        results_df = results_df.where(results_df['probs'] >= threshold).dropna()
 
         return results_df
 
@@ -239,3 +267,5 @@ class G_Classifier():
 
 if __name__ == "__main__":
     g = G_Classifier()
+    print('kg\n', [x[0] for x in g.kg])
+    print(list(g.emerging_relations.index))
